6d4e1473c1832aa10179a28746de655ee0f4fe73
Takahiro Kawashige <lapavoni@gmail.com>
2020-08-03 19:59:39 +0900

    cargo fmt

--- august_leetcoding_challenge/day2_design_hashset.rs
+++ august_leetcoding_challenge/day2_design_hashset.rs
@@ -1,43 +1,39 @@
 struct MyHashSet {
-    data: Vec<i32>
+    data: Vec<i32>,
 }
 
-
-/** 
+/**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl MyHashSet {
-
     /** Initialize your data structure here. */
     fn new() -> Self {
-        MyHashSet {
-            data: Vec::new()
-        }
+        MyHashSet { data: Vec::new() }
     }
-    
+
     fn add(&mut self, key: i32) {
         if self.data.len() == 0 {
             self.data.push(key);
         } else {
             match self.data.binary_search(&key) {
                 Err(i) => self.data.insert(i, key),
-                _ => {},
+                _ => {}
             }
         }
     }
-    
+
     fn remove(&mut self, key: i32) {
         match self.data.binary_search(&key) {
             Ok(i) => {
                 self.data.remove(i);
-            },
+            }
             _ => {
                 // nothing to do
-            },
+            }
         }
     }
-    
+
     /** Returns true if this set contains the specified element */
     fn contains(&self, key: i32) -> bool {
         self.data.contains(&key)
@@ -58,13 +54,13 @@ mod test {
     #[test]
     fn test_day2() {
         let mut hash_set = MyHashSet::new();
-        hash_set.add(1);         
-        hash_set.add(2);         
+        hash_set.add(1);
+        hash_set.add(2);
         assert_eq!(true, hash_set.contains(1));
         assert_eq!(false, hash_set.contains(3));
-        hash_set.add(2);          
+        hash_set.add(2);
         assert_eq!(true, hash_set.contains(2));
-        hash_set.remove(2);          
+        hash_set.remove(2);
         assert_eq!(false, hash_set.contains(2));
     }
 }
