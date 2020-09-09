a82098d8bb786ecedfc416226b0bb321df9bcfe4
Takahiro Kawashige <lapavoni@gmail.com>
2020-09-09 20:25:02 +0900

    Add promlems Pascal's Triangle

--- problems/lib.rs
+++ problems/lib.rs
@@ -86,5 +86,6 @@ pub mod p0111_minimum_depth_of_binary_tree;
 pub mod p0112_path_sum;
 pub mod p0113_path_sum_ii;
 pub mod p0114_flatten_binary_tree_to_linked_list;
+pub mod p0118_pascals_triangle;
 pub mod p0136_single_number;
 pub mod p0169_majority_element;
---
+++ problems/p0118_pascals_triangle.rs
@@ -0,0 +1,42 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let mut results = vec![vec![1], vec![1,1]];
+        match num_rows {
+            0 => vec![],
+            1 => vec![vec![1]],
+            2 => results,
+            _ => {
+                for i in 1..(num_rows as usize - 1) {
+                    let mut row = vec![1];
+                    for j in 0..(results[i].len() - 1) {
+                        row.push(results[i][j] + results[i][j + 1]);
+                    }
+                    row.push(1);
+                    results.push(row);
+                }
+                results
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_0118() {
+        assert_eq!(
+            vec![
+                vec![1],
+                vec![1, 1],
+                vec![1, 2, 1],
+                vec![1, 3, 3, 1],
+                vec![1, 4, 6, 4, 1]
+            ],
+            Solution::generate(5)
+        );
+    }
+}
