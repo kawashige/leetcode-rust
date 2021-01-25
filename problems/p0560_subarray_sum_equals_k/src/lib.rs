1779f146dc5e4682a18db10dccd04f595203e5df
Takahiro Kawashige <lapavoni@gmail.com>
2021-01-25 10:11:10 +0900

    Improve promlems Subarray Sum Equals K

--- problems/p0560_subarray_sum_equals_k/src/lib.rs
+++ problems/p0560_subarray_sum_equals_k/src/lib.rs
@@ -1,21 +1,17 @@
 pub struct Solution {}
 
+use std::collections::HashMap;
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
-        let mut sums = vec![0; nums.len() + 1];
+        let mut map: HashMap<i32, i32> = vec![(0, 1)].into_iter().collect();
         let mut sum = 0;
-        for i in 0..nums.len() {
-            sum += nums[i];
-            sums[i + 1] = sum;
-        }
-
         let mut count = 0;
-        for i in 1..sums.len() {
-            for j in 0..i {
-                if sums[i] - sums[j] == k {
-                    count += 1;
-                }
+        for n in nums {
+            sum += n;
+            if let Some(v) = map.get(&(sum - k)) {
+                count += v;
             }
+            *map.entry(sum).or_insert(0) += 1;
         }
         count
     }
