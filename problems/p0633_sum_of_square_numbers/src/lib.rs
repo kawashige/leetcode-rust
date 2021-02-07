855afb583c856c292981156764bf5e11d24dce06
Takahiro Kawashige <lapavoni@gmail.com>
2021-02-07 14:44:07 +0900

    Update promlems Sum of Square Numbers

--- problems/p0633_sum_of_square_numbers/src/lib.rs
+++ problems/p0633_sum_of_square_numbers/src/lib.rs
@@ -1,20 +1,23 @@
-use std::collections::HashSet;
 pub struct Solution {}
 
 impl Solution {
     pub fn judge_square_sum(c: i32) -> bool {
-        let mut square_numbers = HashSet::new();
-        for i in 0..=c {
-            let s = i * i;
-            if c < s {
-                break;
+        let mut c = c;
+        let mut i: i32 = 2;
+        while i.saturating_mul(i) <= c {
+            let mut count = 0;
+            if c % i == 0 {
+                while c % i == 0 {
+                    count += 1;
+                    c /= i;
+                }
+                if i % 4 == 3 && count % 2 != 0 {
+                    return false;
+                }
             }
-            square_numbers.insert(s);
-            if c - s <= s && square_numbers.contains(&(c - s)) {
-                return true;
-            }
+            i += 1;
         }
-        false
+        c % 4 != 3
     }
 }
 
@@ -24,6 +27,7 @@ mod test {
 
     #[test]
     fn test_0633() {
+        assert!(!Solution::judge_square_sum(15));
         assert!(Solution::judge_square_sum(5));
         assert!(!Solution::judge_square_sum(3));
         assert!(Solution::judge_square_sum(4));
