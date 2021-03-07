959ced6910cdfb5b8abb093cbf187fbe2a686b44
Takahiro Kawashige <lapavoni@gmail.com>
2021-03-07 16:17:56 +0900

    Update problems Accounts Merge

--- problems/p0721_accounts_merge/src/lib.rs
+++ problems/p0721_accounts_merge/src/lib.rs
@@ -1,40 +1,90 @@
-use std::collections::{BTreeSet, HashMap};
+use std::collections::HashMap;
 pub struct Solution {}
+
+pub struct DisjointSet {
+    parent: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl DisjointSet {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+        }
+    }
+
+    pub fn root(&mut self, i: usize) -> usize {
+        if self.parent[i] == i {
+            return i;
+        }
+
+        let parent = self.root(self.parent[i]);
+        self.parent[i] = parent;
+        parent
+    }
+
+    pub fn unite(&mut self, i: usize, j: usize) {
+        let mut parent_i = self.root(i);
+        let mut parent_j = self.root(j);
+
+        if parent_i == parent_j {
+            return;
+        }
+
+        if self.size(parent_i) > self.size(parent_j) {
+            std::mem::swap(&mut parent_i, &mut parent_j);
+        }
+
+        self.parent[parent_j] = parent_i;
+        self.size[parent_i] += self.size[parent_j];
+    }
+
+    fn size(&mut self, i: usize) -> usize {
+        let root = self.root(i);
+        self.size[root]
+    }
+}
 
 impl Solution {
     pub fn accounts_merge(accounts: Vec<Vec<String>>) -> Vec<Vec<String>> {
-        let mut map: HashMap<String, Vec<BTreeSet<String>>> = HashMap::new();
+        let mut disjoint_set = DisjointSet::new(10001);
 
+        let mut email_to_name = HashMap::new();
+        let mut email_to_id = HashMap::new();
+
+        let mut id = 0;
         for mut account in accounts {
             let name = account.remove(0);
-            let mut addresses = account.into_iter().collect::<BTreeSet<String>>();
-            if let Some(sets) = map.get_mut(&name) {
-                let mut next = vec![];
-                for mut set in sets.clone() {
-                    if set.is_disjoint(&addresses) {
-                        next.push(set);
-                    } else {
-                        addresses.append(&mut set);
-                    }
+            let first_email = account[0].clone();
+            for email in account {
+                email_to_name.entry(email.clone()).or_insert(name.clone());
+                if email_to_id.get(&email).is_none() {
+                    email_to_id.insert(email.clone(), id);
+                    id += 1;
                 }
-                next.push(addresses);
-                *sets = next;
-            } else {
-                map.insert(name, vec![addresses]);
+                disjoint_set.unite(email_to_id[&first_email], email_to_id[&email])
             }
         }
 
-        let mut result = Vec::new();
-        for (k, addresses) in map.into_iter() {
-            for address in addresses {
-                result.push(
-                    std::iter::once(k.clone())
-                        .chain(address.into_iter())
-                        .collect(),
-                );
-            }
+        let mut result = HashMap::new();
+        for email in email_to_name.keys() {
+            (*result
+                .entry(disjoint_set.root(email_to_id[email]))
+                .or_insert(Vec::new()))
+            .push(email.clone());
         }
+
         result
+            .values()
+            .map(|v| {
+                let mut v: Vec<String> = v.iter().cloned().collect();
+                v.sort_unstable();
+                let mut r = vec![email_to_name[&v[0]].clone()];
+                r.append(&mut v);
+                r
+            })
+            .collect()
     }
 }
 
@@ -60,6 +110,7 @@ mod test {
                 vec!["Mary".to_string(), "mary@mail.com".to_string()]
             ]),
             vec![
+                vec!["Mary".to_string(), "mary@mail.com".to_string()],
                 vec!["John".to_string(), "johnnybravo@mail.com".to_string()],
                 vec![
                     "John".to_string(),
@@ -67,7 +118,6 @@ mod test {
                     "john_newyork@mail.com".to_string(),
                     "johnsmith@mail.com".to_string()
                 ],
-                vec!["Mary".to_string(), "mary@mail.com".to_string()]
             ]
         )
     }
