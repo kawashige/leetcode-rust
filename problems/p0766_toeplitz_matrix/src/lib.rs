7837bfde6356aaee385930a639f497d1f3bcc74c
Takahiro Kawashige <lapavoni@gmail.com>
2021-03-25 18:48:12 +0900

    Add problems Toeplitz Matrix

---
+++ problems/p0766_toeplitz_matrix/src/lib.rs
@@ -0,0 +1,29 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_toeplitz_matrix(matrix: Vec<Vec<i32>>) -> bool {
+        for i in 1..matrix.len() {
+            for j in 1..matrix[0].len() {
+                if matrix[i][j] != matrix[i - 1][j - 1] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0766() {
+        assert!(Solution::is_toeplitz_matrix(vec![
+            vec![1, 2, 3, 4],
+            vec![5, 1, 2, 3],
+            vec![9, 5, 1, 2]
+        ]));
+        assert!(!Solution::is_toeplitz_matrix(vec![vec![1, 2], vec![2, 2]]));
+    }
+}
