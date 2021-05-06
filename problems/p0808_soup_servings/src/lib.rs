2d10ac10adab8c1f4d593ee6173d41865abde8bf
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-06 16:18:49 +0900

    Update problems Soup Servings

--- problems/p0808_soup_servings/src/lib.rs
+++ problems/p0808_soup_servings/src/lib.rs
@@ -1,30 +1,44 @@
-use std::collections::HashMap;
-
 pub struct Solution {}
 
 impl Solution {
-    pub fn recurse(a: i32, b: i32, memo: &mut HashMap<(i32, i32), f64>) -> f64 {
-        if let Some(p) = memo.get(&(a, b)) {
-            *p
-        } else if a == 0 && b > 0 {
-            1.0
-        } else if a == 0 && b == 0 {
-            0.5
-        } else if b == 0 {
-            0.0
-        } else {
-            let r = 0.25
-                * (Self::recurse(std::cmp::max(0, a - 100), b, memo)
-                    + Self::recurse(std::cmp::max(0, a - 75), std::cmp::max(0, b - 25), memo)
-                    + Self::recurse(std::cmp::max(0, a - 50), std::cmp::max(0, b - 50), memo)
-                    + Self::recurse(std::cmp::max(0, a - 25), std::cmp::max(0, b - 75), memo));
-            memo.insert((a, b), r);
-            r
-        }
-    }
-
     pub fn soup_servings(n: i32) -> f64 {
-        Self::recurse(n, n, &mut HashMap::new())
+        fn m(a: usize, b: usize) -> usize {
+            if a >= b {
+                a - b
+            } else {
+                0
+            }
+        }
+
+        let n = ((n + 24) / 25) as usize;
+        if n >= 500 {
+            return 1.0;
+        }
+
+        let mut memo = vec![vec![0.0; n + 1]; n + 1];
+
+        for s in 0..=(2 * n) {
+            for i in 0..=n {
+                if s < i || s - i > n {
+                    continue;
+                }
+                let j = s - i;
+                let r = if i == 0 && j == 0 {
+                    0.5
+                } else if i == 0 {
+                    1.0
+                } else if i > 0 && j > 0 {
+                    0.25 * (memo[m(i, 4)][j]
+                        + memo[m(i, 3)][m(j, 1)]
+                        + memo[m(i, 2)][m(j, 2)]
+                        + memo[m(i, 1)][m(j, 3)])
+                } else {
+                    0.0
+                };
+                memo[i][j] = r;
+            }
+        }
+        memo[n][n]
     }
 }
 
@@ -35,5 +49,6 @@ mod test {
     #[test]
     fn test_0808() {
         assert_eq!(Solution::soup_servings(50), 0.625);
+        assert_eq!(Solution::soup_servings(660295675), 1.0);
     }
 }
