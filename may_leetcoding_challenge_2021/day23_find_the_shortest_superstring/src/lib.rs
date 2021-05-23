3c054a8a9014ac0f60e893db4867cddf129a1f97
Takahiro Kawashige <lapavoni@gmail.com>
2021-05-23 18:47:21 +0900

    Update may_leetcoding_challenge_2021 day23 Find the Shortest Superstring

--- may_leetcoding_challenge_2021/day23_find_the_shortest_superstring/src/lib.rs
+++ may_leetcoding_challenge_2021/day23_find_the_shortest_superstring/src/lib.rs
@@ -1,58 +1,85 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn dfs(
-        words: &Vec<String>,
-        overlapped: &Vec<Vec<usize>>,
-        prev_i: Option<usize>,
-        used: usize,
-        min_s: &mut String,
-        s: String,
-    ) {
-        if used == (1 << words.len()) - 1 {
-            *min_s = s.clone();
-            return;
+    pub fn recurse(
+        dp: &mut Vec<Vec<i32>>,
+        cost: &Vec<Vec<i32>>,
+        pre: &mut Vec<Vec<usize>>,
+        bit: usize,
+        v: usize,
+    ) -> i32 {
+        if dp[bit][v] != -1 {
+            return dp[bit][v];
         }
 
-        for i in 0..words.len() {
-            if used & 1 << i > 0 {
+        if bit == 1 << v {
+            return cost[v][v];
+        }
+
+        let mut result = 10_000;
+        let prev = bit & !(1 << v);
+
+        for u in 0..dp[0].len() {
+            if prev & 1 << u == 0 {
                 continue;
             }
 
-            let s = format!(
-                "{}{}",
-                s,
-                &words[i][prev_i.map_or_else(|| 0, |j| overlapped[j][i])..]
-            );
-
-            if s.len() > min_s.len() {
-                continue;
+            if result > Self::recurse(dp, cost, pre, prev, u) + cost[u][v] {
+                result = Self::recurse(dp, cost, pre, prev, u) + cost[u][v];
+                pre[bit][v] = u;
             }
-
-            Self::dfs(words, overlapped, i.into(), used | 1 << i, min_s, s);
         }
+
+        dp[bit][v] = result;
+        result
     }
 
     pub fn shortest_superstring(words: Vec<String>) -> String {
-        let mut overlapped = vec![vec![0; words.len()]; words.len()];
+        let mut cost = vec![vec![0; words.len()]; words.len()];
         for i in 0..words.len() {
             for j in 0..words.len() {
                 if i == j {
-                    continue;
+                    cost[i][j] = words[i].len() as i32;
+                } else {
+                    let bytes_i = words[i].as_bytes();
+                    let bytes_j = words[j].as_bytes();
+                    cost[i][j] = (bytes_j.len()
+                        - (1..std::cmp::min(bytes_i.len(), bytes_j.len()))
+                            .rev()
+                            .find(|k| bytes_i[(bytes_i.len() - k)..] == bytes_j[..*k])
+                            .unwrap_or(0)) as i32;
                 }
-                let bytes_i = words[i].as_bytes();
-                let bytes_j = words[j].as_bytes();
-                overlapped[i][j] = (1..std::cmp::min(words[i].len(), words[j].len()))
-                    .rev()
-                    .find(|k| bytes_i[(bytes_i.len() - k)..] == bytes_j[..*k])
-                    .unwrap_or(0);
             }
         }
 
-        // println!("{:?}", overlapped);
+        let mut dp = vec![vec![-1; words.len()]; 1 << words.len()];
+        let mut pre = vec![vec![0; words.len()]; 1 << words.len()];
 
-        let mut s = std::iter::repeat(".").take(20 * 12 + 1).collect();
-        Self::dfs(&words, &overlapped, None, 0, &mut s, String::new());
+        let mut result = 10_000;
+        let mut id = 0;
+        for v in 0..words.len() {
+            if result > Self::recurse(&mut dp, &cost, &mut pre, (1 << words.len()) - 1, v) {
+                result = Self::recurse(&mut dp, &cost, &mut pre, (1 << words.len()) - 1, v);
+                id = v;
+            }
+        }
+
+        let mut bit = (1 << words.len()) - 1;
+        let mut order = Vec::with_capacity(words.len());
+
+        while bit > 0 {
+            order.push(id);
+            let tmp = id;
+            id = pre[bit][id];
+            bit &= !(1 << tmp);
+        }
+
+        order = order.into_iter().rev().collect();
+        let mut s = words[order[0]].clone();
+        for i in 1..order.len() {
+            s +=
+                &words[order[i]][(words[order[i]].len() - cost[order[i - 1]][order[i]] as usize)..];
+        }
         s
     }
 }
@@ -63,15 +90,6 @@ mod test {
 
     #[test]
     fn test_day23() {
-        assert_eq!(
-            Solution::shortest_superstring(vec![
-                "sssv".to_string(),
-                "svq".to_string(),
-                "dskss".to_string(),
-                "sksss".to_string()
-            ]),
-            "dsksssvq".to_string()
-        );
         assert_eq!(
             Solution::shortest_superstring(vec![
                 "alex".to_string(),
