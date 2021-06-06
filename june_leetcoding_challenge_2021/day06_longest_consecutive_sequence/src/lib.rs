45390ad57fe5d748bad7be884f4abeadd685352e
Takahiro Kawashige <lapavoni@gmail.com>
2021-06-06 18:32:31 +0900

    Update june_leetcoding_challenge_2021 day06 Longest Consecutive Sequence

--- june_leetcoding_challenge_2021/day06_longest_consecutive_sequence/src/lib.rs
+++ june_leetcoding_challenge_2021/day06_longest_consecutive_sequence/src/lib.rs
@@ -1,44 +1,24 @@
+use std::collections::HashSet;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        if nums.len() < 2 {
-            return nums.len() as i32;
-        }
-
-        let n = nums.len();
-        let min = nums.iter().min().unwrap();
-        let mut bucket = vec![vec![]; 10];
-
-        for x in &nums {
-            let num = (x - min) as usize;
-            bucket[num % 10].push((num / 10, num));
-        }
-
-        while bucket[0].len() != n {
-            let mut new_bucket = vec![vec![]; 10];
-            for i in 0..10 {
-                for j in 0..bucket[i].len() {
-                    new_bucket[bucket[i][j].0 % 10].push((bucket[i][j].0 / 10, bucket[i][j].1));
-                }
-            }
-            bucket = new_bucket;
-        }
-
+        let set = nums.into_iter().collect::<HashSet<i32>>();
         let mut max = 0;
-        let mut count = 1;
-        for i in 1..n {
-            if bucket[0][i].1 == bucket[0][i - 1].1 {
-                continue;
-            } else if bucket[0][i].1 == bucket[0][i - 1].1 + 1 {
-                count += 1;
-            } else {
+        for n in &set {
+            if !set.contains(&(n - 1)) {
+                let mut num = *n;
+                let mut count = 1;
+
+                while set.contains(&(num + 1)) {
+                    num += 1;
+                    count += 1;
+                }
                 max = std::cmp::max(max, count);
-                count = 1;
             }
         }
-
-        std::cmp::max(max, count)
+        max
     }
 }
 
