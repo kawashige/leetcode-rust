048c3c2555287e56026d382600041c7a950e833a
Takahiro Kawashige <lapavoni@gmail.com>
2021-11-16 21:08:41 +0900

    Update problems Kth Smallest Number in Multiplication Table

--- problems/p0668_kth_smallest_number_in_multiplication_table/src/lib.rs
+++ problems/p0668_kth_smallest_number_in_multiplication_table/src/lib.rs
@@ -2,20 +2,19 @@ pub struct Solution {}
 
 impl Solution {
     pub fn find_kth_number(m: i32, n: i32, k: i32) -> i32 {
-        let mut count = vec![0; (n * m) as usize + 1];
-        let mut sum = 0;
-        for i in 1..=count.len() {
-            if i <= m as usize {
-                for j in (i..=(i * n as usize)).step_by(i) {
-                    count[j] += 1;
-                }
-            }
-            sum += count[i];
-            if k <= sum {
-                return i as i32;
+        let mut ng = 0;
+        let mut ok = m * n;
+
+        while ng + 1 < ok {
+            let mid = (ng + ok) / 2;
+            if k <= (1..=m).fold(0, |acc, i| acc + (mid / i).min(n)) {
+                ok = mid;
+            } else {
+                ng = mid;
             }
         }
-        -1
+
+        ok
     }
 }
 
@@ -25,7 +24,7 @@ mod test {
 
     #[test]
     fn test_0668() {
-        assert_eq!(Solution::find_kth_number(9895, 28045, 100787757), 3);
+        assert_eq!(Solution::find_kth_number(9895, 28045, 100787757), 31852588);
         assert_eq!(Solution::find_kth_number(3, 3, 5), 3);
         assert_eq!(Solution::find_kth_number(2, 3, 6), 6);
     }
