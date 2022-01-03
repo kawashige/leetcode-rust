e035761524e8a6494a791446dac668144e27f6d1
Takahiro Kawashige <lapavoni@gmail.com>
2022-01-03 10:26:41 +0900

    Add problems Longest Well-Performing Interval

---
+++ problems/p1124_longest_well_performing_interval/src/lib.rs
@@ -0,0 +1,31 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn longest_wpi(hours: Vec<i32>) -> i32 {
+        let acc = std::iter::once(0)
+            .chain(hours.into_iter())
+            .scan(0, |sum, x| {
+                *sum += if 8 < x { 1 } else { -1 };
+                Some(*sum)
+            })
+            .collect::<Vec<_>>();
+
+        println!("{:?}", acc);
+
+        (1..acc.len())
+            .map(|i| i - (0..i).find(|j| acc[i] - acc[*j] > 0).unwrap_or(i))
+            .max()
+            .unwrap() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1124() {
+        assert_eq!(Solution::longest_wpi(vec![9, 9, 6, 0, 6, 6, 9]), 3);
+        assert_eq!(Solution::longest_wpi(vec![6, 6, 6]), 0);
+    }
+}
