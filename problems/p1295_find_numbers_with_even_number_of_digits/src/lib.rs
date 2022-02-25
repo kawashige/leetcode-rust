9385c52247e9c2fbfd9215723e7d500c4685c20c
Takahiro Kawashige <lapavoni@gmail.com>
2022-02-25 09:06:50 +0900

    Add problems Find Numbers with Even Number of Digits

---
+++ problems/p1295_find_numbers_with_even_number_of_digits/src/lib.rs
@@ -0,0 +1,18 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn find_numbers(nums: Vec<i32>) -> i32 {
+        nums.iter().filter(|x| x.to_string().len() % 2 == 0).count() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1295() {
+        assert_eq!(Solution::find_numbers(vec![12, 345, 2, 6, 7896]), 2);
+        assert_eq!(Solution::find_numbers(vec![555, 901, 482, 1771]), 1);
+    }
+}
