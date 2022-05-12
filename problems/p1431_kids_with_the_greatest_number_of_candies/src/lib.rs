6e6ba5f5cb4359532406b4139f843fd0f891b91a
Takahiro Kawashige <lapavoni@gmail.com>
2022-05-12 09:09:28 +0900

    Add problems Kids With the Greatest Number of Candies

---
+++ problems/p1431_kids_with_the_greatest_number_of_candies/src/lib.rs
@@ -0,0 +1,32 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        let max = candies.iter().max().unwrap();
+        candies
+            .iter()
+            .map(|c| max <= &(c + extra_candies))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1431() {
+        assert_eq!(
+            Solution::kids_with_candies(vec![2, 3, 5, 1, 3], 3),
+            vec![true, true, true, false, true]
+        );
+        assert_eq!(
+            Solution::kids_with_candies(vec![4, 2, 1, 1, 2], 1),
+            vec![true, false, false, false, false]
+        );
+        assert_eq!(
+            Solution::kids_with_candies(vec![12, 1, 12], 10),
+            vec![true, false, true]
+        );
+    }
+}
