5ffe8c5e86b7fec21d296611ba27f9b0e446bb48
Takahiro Kawashige <lapavoni@gmail.com>
2022-06-06 09:18:06 +0900

    Add problems Minimum Number of Days to Make m Bouquets

---
+++ problems/p1482_minimum_number_of_days_to_make_m_bouquets/src/lib.rs
@@ -0,0 +1,54 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn ok(bloom_day: &[i32], day: i32, k: i32, m: i32) -> bool {
+        let mut bouquets = 0;
+        let mut adjacent = 0;
+
+        for x in bloom_day {
+            if x <= &day {
+                adjacent += 1;
+                if adjacent == k {
+                    bouquets += 1;
+                    adjacent = 0;
+                }
+            } else {
+                adjacent = 0;
+            }
+        }
+
+        m <= bouquets
+    }
+
+    pub fn min_days(bloom_day: Vec<i32>, m: i32, k: i32) -> i32 {
+        if (bloom_day.len() as i32) < m * k {
+            return -1;
+        }
+
+        let mut ok = *bloom_day.iter().max().unwrap();
+        let mut ng = 0;
+
+        while ng + 1 < ok {
+            let mid = (ng + ok) / 2;
+            if Self::ok(&bloom_day, mid, k, m) {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+
+        ok
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1482() {
+        assert_eq!(Solution::min_days(vec![1, 10, 3, 10, 2], 3, 1), 3);
+        assert_eq!(Solution::min_days(vec![1, 10, 3, 10, 2], 3, 2), -1);
+        assert_eq!(Solution::min_days(vec![7, 7, 7, 7, 12, 7, 7], 2, 3), 12);
+    }
+}
