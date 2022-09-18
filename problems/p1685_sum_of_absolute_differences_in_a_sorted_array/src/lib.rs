710c4c2e63282225dd44431648280f6bde180872
Takahiro Kawashige <lapavoni@gmail.com>
2022-09-18 09:42:54 +0900

    Add problems Sum of Absolute Differences in a Sorted Array

---
+++ problems/p1685_sum_of_absolute_differences_in_a_sorted_array/src/lib.rs
@@ -0,0 +1,44 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn get_sum_absolute_differences(nums: Vec<i32>) -> Vec<i32> {
+        let mut acc = vec![0; nums.len()];
+        acc[0] = nums[0];
+        for i in 1..nums.len() {
+            acc[i] += acc[i - 1] + nums[i];
+        }
+
+        println!("{:?}", acc);
+
+        (0..nums.len())
+            .map(|i| {
+                let mut diff = 0;
+                if 0 < i {
+                    diff += nums[i] * i as i32 - acc[i - 1];
+                }
+                if i < acc.len() - 1 {
+                    diff +=
+                        acc[acc.len() - 1] - acc[i] - nums[i] * (acc.len() as i32 - (i as i32 + 1));
+                }
+                diff
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1685() {
+        assert_eq!(
+            Solution::get_sum_absolute_differences(vec![2, 3, 5]),
+            vec![4, 3, 5]
+        );
+        assert_eq!(
+            Solution::get_sum_absolute_differences(vec![1, 4, 6, 8, 10]),
+            vec![24, 15, 13, 15, 21]
+        );
+    }
+}
