e15304ba782518851747f49a1be27db3a3ac68e9
Takahiro Kawashige <lapavoni@gmail.com>
2022-11-05 10:33:17 +0900

    Add problems Largest Merge Of Two Strings

---
+++ problems/p1754_largest_merge_of_two_strings/src/lib.rs
@@ -0,0 +1,38 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn largest_merge(word1: String, word2: String) -> String {
+        let mut merge = String::new();
+        let mut i1 = 0;
+        let mut i2 = 0;
+
+        while i1 < word1.len() || i2 < word2.len() {
+            if &word1[i1..] < &word2[i2..] {
+                merge.push(word2.as_bytes()[i2] as char);
+                i2 += 1
+            } else {
+                merge.push(word1.as_bytes()[i1] as char);
+                i1 += 1
+            }
+        }
+
+        merge
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1754() {
+        assert_eq!(
+            Solution::largest_merge("cabaa".to_string(), "bcaaa".to_string()),
+            "cbcabaaaaa".to_string()
+        );
+        assert_eq!(
+            Solution::largest_merge("abcabc".to_string(), "abdcaba".to_string()),
+            "abdcabcabcaba".to_string()
+        );
+    }
+}
