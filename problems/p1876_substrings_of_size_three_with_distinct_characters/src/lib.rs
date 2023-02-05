78aaf5112b88f08d89925f65ca1bd87f0ff5c0a8
Takahiro Kawashige <lapavoni@gmail.com>
2023-02-05 09:07:20 +0900

    Add problems Substrings of Size Three with Distinct Characters

---
+++ problems/p1876_substrings_of_size_three_with_distinct_characters/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn count_good_substrings(s: String) -> i32 {
+        // if s.len() < 3 {
+        //     return 0;
+        // }
+        s.as_bytes()
+            .windows(3)
+            .filter(|bytes| bytes[0] != bytes[1] && bytes[1] != bytes[2] && bytes[2] != bytes[0])
+            .count() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_1876() {
+        assert_eq!(Solution::count_good_substrings("x".to_string()), 0);
+        assert_eq!(Solution::count_good_substrings("xyzzaz".to_string()), 1);
+        assert_eq!(Solution::count_good_substrings("aababcabc".to_string()), 4);
+    }
+}
