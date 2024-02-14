34782deb838814c2060ba4c09538571ea169cf0c
Takahiro Kawashige <lapavoni@gmail.com>
2024-02-14 09:14:22 +0900

    Add problems Strictly Palindromic Number

---
+++ problems/p2396_strictly_palindromic_number/src/lib.rs
@@ -0,0 +1,45 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_strictly_palindromic(n: i32) -> bool {
+        for b in 2..n - 1 {
+            let mut digits = Vec::new();
+
+            let mut remains = n;
+            let mut div = 1;
+            while div * b <= n {
+                div *= b;
+            }
+
+            while 0 < div {
+                if 0 < remains / div {
+                    digits.push(remains / div);
+                    remains = remains % div;
+                } else {
+                    digits.push(0);
+                }
+                div /= b;
+            }
+
+            println!("{}: {:?}", b, digits);
+
+            if (0..digits.len()).any(|i| digits[i] != digits[digits.len() - 1 - i]) {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_2396() {
+        assert!(!Solution::is_strictly_palindromic(9));
+        assert!(!Solution::is_strictly_palindromic(4));
+        assert!(!Solution::is_strictly_palindromic(100000));
+    }
+}
