51619818a342f28b9b922359cc666e03e89aad69
Takahiro Kawashige <lapavoni@gmail.com>
2024-02-19 09:11:34 +0900

    Add problems Check Distances Between Same Letters

---
+++ problems/p2399_check_distances_between_same_letters/src/lib.rs
@@ -0,0 +1,45 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn check_distances(s: String, distance: Vec<i32>) -> bool {
+        let dist = s
+            .as_bytes()
+            .iter()
+            .enumerate()
+            .fold([100; 26], |mut dist, (i, b)| {
+                if dist[(b - b'a') as usize] == 100 {
+                    dist[(b - b'a') as usize] = i;
+                } else {
+                    dist[(b - b'a') as usize] = i - dist[(b - b'a') as usize] - 1;
+                }
+                dist
+            });
+
+        println!("{:?}", dist);
+        (0..distance.len()).all(|i| dist[i] == 100 || dist[i] as i32 == distance[i])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_2399() {
+        assert!(Solution::check_distances(
+            "tffyjlpvhxpigmzysnwkquhxrtbosocqjzniwebvcelgukrm".to_string(),
+            vec![
+                15, 11, 9, 11, 3, 0, 30, 13, 23, 27, 25, 36, 33, 16, 1, 3, 10, 21, 11, 24, 22, 31,
+                17, 13, 11, 18
+            ]
+        ));
+        assert!(Solution::check_distances(
+            "abaccb".to_string(),
+            vec![1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ));
+        assert!(!Solution::check_distances(
+            "aa".to_string(),
+            vec![1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ));
+    }
+}
