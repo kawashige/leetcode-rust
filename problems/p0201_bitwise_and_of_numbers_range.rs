cd181bd22c7ca297e7f9c1b4924628c2657f7ff5
Takahiro Kawashige <lapavoni@gmail.com>
2024-02-21 09:21:37 +0900

    Update problems Bitwise AND of Numbers Range

--- problems/p0201_bitwise_and_of_numbers_range.rs
+++ problems/p0201_bitwise_and_of_numbers_range.rs
@@ -2,12 +2,12 @@ pub struct Solution {}
 
 impl Solution {
     pub fn range_bitwise_and(m: i32, n: i32) -> i32 {
-        let mut result = std::i32::MAX;
-        for i in m..=n {
-            if result == 0 {
+        let mut result = 0;
+        for i in (0..31).rev() {
+            if m & 1 << i != n & 1 << i {
                 break;
             }
-            result &= i;
+            result |= m & 1 << i;
         }
         result
     }
@@ -23,5 +23,9 @@ mod test {
         assert_eq!(0, Solution::range_bitwise_and(0, 1));
         assert_eq!(0, Solution::range_bitwise_and(0, 0));
         assert_eq!(0, Solution::range_bitwise_and(1, std::i32::MAX));
+        assert_eq!(
+            1073741824,
+            Solution::range_bitwise_and(1073741824, 2147483647)
+        );
     }
 }
