3c820d63566f24683161a31a9011296c21f90206
Takahiro Kawashige <lapavoni@gmail.com>
2024-03-20 12:51:25 +0900

    Add problems Count Number of Distinct Integers After Reverse Operations

---
+++ problems/p2442_count_number_of_distinct_integers_after_reverse_operations/src/lib.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn reverse(num: i32) -> i32 {
+        let mut num = num;
+        let mut reversed = 0;
+        while 0 < num {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        reversed
+    }
+    pub fn count_distinct_integers(nums: Vec<i32>) -> i32 {
+        let mut set = HashSet::new();
+        for num in nums {
+            set.insert(Self::reverse(num));
+            set.insert(num);
+        }
+        set.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_2442() {
+        assert_eq!(
+            Solution::count_distinct_integers(vec![1, 13, 10, 12, 31]),
+            6
+        );
+        assert_eq!(Solution::count_distinct_integers(vec![2, 2, 2]), 1);
+    }
+}
