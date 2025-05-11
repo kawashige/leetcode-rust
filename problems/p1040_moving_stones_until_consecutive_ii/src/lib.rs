0d2ba4e503f6f1d28851baf4e46e0d2a6dd5356a
Takahiro Kawashige <lapavoni@gmail.com>
2025-05-11 14:41:50 +0900

    Add problems Moving Stones Until Consecutive II

--- problems/p1040_moving_stones_until_consecutive_ii/src/lib.rs
+++ problems/p1040_moving_stones_until_consecutive_ii/src/lib.rs
@@ -1,9 +1,29 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn num_moves_stones_ii(mut stones: Vec<i32>) -> Vec<i32> {
+    pub fn num_moves_stones_ii(stones: Vec<i32>) -> Vec<i32> {
+        let mut stones = stones;
         stones.sort_unstable();
-        stones
+
+        let mut min = std::i32::MAX;
+        let mut l = 0;
+        for i in 0..stones.len() {
+            while (stones.len() as i32) < stones[i] - stones[l] + 1 {
+                l += 1;
+            }
+            if i - l + 1 == stones.len() - 1 && stones[i] - stones[l] + 1 == stones.len() as i32 - 1
+            {
+                min = min.min(2)
+            } else {
+                min = min.min((stones.len() - (i - l + 1)) as i32);
+            }
+        }
+
+        let max = (stones[stones.len() - 2] - stones[0] + 2)
+            .max(stones[stones.len() - 1] - stones[1] + 2)
+            - stones.len() as i32;
+
+        vec![min, max]
     }
 }
 
