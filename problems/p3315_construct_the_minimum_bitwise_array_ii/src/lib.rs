332a18c668f0b4298ae95c16929750a8676711dc
Takahiro Kawashige <lapavoni@gmail.com>
2025-11-19 09:18:42 +0900

    Add problems Construct the Minimum Bitwise Array II

---
+++ problems/p3315_construct_the_minimum_bitwise_array_ii/src/lib.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
+        nums.into_iter()
+            .map(|n| {
+                if n == 2 {
+                    return -1;
+                }
+                let mut i = 0;
+                while n & 1 << i != 0 {
+                    i += 1;
+                }
+                n ^ 1 << (i - 1)
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_3315() {
+        assert_eq!(
+            Solution::min_bitwise_array(vec![2, 3, 5, 7]),
+            vec![-1, 1, 4, 3]
+        );
+        assert_eq!(
+            Solution::min_bitwise_array(vec![11, 13, 31]),
+            vec![9, 12, 15]
+        );
+    }
+}
