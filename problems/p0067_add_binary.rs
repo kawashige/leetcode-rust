80966f503cbd804c6790d3963fb3f99492cb0ece
Takahiro Kawashige <lapavoni@gmail.com>
2026-02-15 09:18:22 +0900

    Update problems Add Binary

--- problems/p0067_add_binary.rs
+++ problems/p0067_add_binary.rs
@@ -1,10 +1,28 @@
+use std::collections::VecDeque;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
-        let a = i128::from_str_radix(&a, 2).unwrap();
-        let b = i128::from_str_radix(&b, 2).unwrap();
-        format!("{:b}", a + b)
+        let mut c = 0;
+        let mut queue = VecDeque::new();
+        for i in 0..a.len().max(b.len()) {
+            let b = if a.len() <= i {
+                0
+            } else {
+                (a.as_bytes()[a.len() - 1 - i] - b'0') as i32
+            } + if b.len() <= i {
+                0
+            } else {
+                (b.as_bytes()[b.len() - 1 - i] - b'0') as i32
+            } + c;
+            c = b / 2;
+            queue.push_front(if b % 2 == 0 { '0' } else { '1' });
+        }
+        if c == 1 {
+            queue.push_front('1');
+        }
+        queue.into_iter().collect()
     }
 }
 
