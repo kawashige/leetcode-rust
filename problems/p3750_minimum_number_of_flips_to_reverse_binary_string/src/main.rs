353620147d37d522755a64129acfb34b17ce2c60
Takahiro Kawashige <lapavoni@gmail.com>
2026-06-29 12:44:11 +0900

    Add Problem Minimum Number of Flips to Reverse Binary String

---
+++ problems/p3750_minimum_number_of_flips_to_reverse_binary_string/src/main.rs
@@ -0,0 +1,25 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn minimum_flips(n: i32) -> i32 {
+        let s = format!("{:b}", n).trim_start_matches('0').to_string();
+        println!("{:?}", s);
+        (0..s.len() / 2)
+            .filter(|i| s.as_bytes()[*i] != s.as_bytes()[s.len() - 1 - i])
+            .count() as i32
+            * 2
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_3750() {
+        assert_eq!(Solution::minimum_flips(7), 0);
+        assert_eq!(Solution::minimum_flips(10), 4);
+    }
+}
+
+fn main() {}
